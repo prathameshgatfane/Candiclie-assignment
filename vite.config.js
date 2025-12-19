36b70b9010e177fe5c9920421f2f598426353232
prathameshgatfane <prathameshgatfane2019@gmail.com>
2025-12-19 14:19:59 +0530

    Set Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  base: '/Candiclie-assignment/', // important for GitHub Pages
 })
